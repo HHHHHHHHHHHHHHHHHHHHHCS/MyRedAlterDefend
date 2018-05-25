e13a7c38d2df299557b1a1fce56a9d834724f500
HCS <464962683@qq.com>
2018-05-26 00:20:03 +0800

    桥接学习完成

--- Assets/My/DesignMode/02Bridge/_02Bridge.cs
+++ Assets/My/DesignMode/02Bridge/_02Bridge.cs
@@ -6,15 +6,88 @@ public class _02Bridge : MonoBehaviour
 {
     private void Start()
     {
-        _02Sphere sphere = new _02Sphere();
+        IRenderEngine renderEngine = new _SuperRender();
+
+        _02Sphere sphere = new _02Sphere(renderEngine);
         sphere.Draw();
-        _02Cube cube = new _02Cube();
+        _02Cube cube = new _02Cube(renderEngine);
         cube.Draw();
-        _02Capsule capsule = new _02Capsule();
+        _02Capsule capsule = new _02Capsule(renderEngine);
         capsule.Draw();
     }
 }
 
+public abstract class Abs_Shape
+{
+    public string name;
+    public IRenderEngine renderEngine;
+
+    public Abs_Shape(IRenderEngine _renderEngine)
+    {
+        renderEngine = _renderEngine;
+    }
+
+    public void Draw()
+    {
+        renderEngine.Render(name);
+    }
+}
+
+
+public interface IRenderEngine
+{
+    void Render(string name);
+}
+
+public class _02Capsule : Abs_Shape
+{
+    public _02Capsule(IRenderEngine _renderEngine) : base(_renderEngine)
+    {
+        name = "Capsule";
+    }
+}
+
+public class _02Cube : Abs_Shape
+{
+    public _02Cube(IRenderEngine _renderEngine) : base(_renderEngine)
+    {
+        name = "Cube";
+    }
+}
+
+public class _02Sphere : Abs_Shape
+{
+    public _02Sphere(IRenderEngine _renderEngine) : base(_renderEngine)
+    {
+        name = "Sphere";
+    }
+}
+
+public class _OpenGL : IRenderEngine
+{
+    public void Render(string name)
+    {
+        Debug.Log("OpenGL绘制" + name);
+    }
+}
+
+public class _DirectX : IRenderEngine
+{
+    public void Render(string name)
+    {
+        Debug.Log("DirectX绘制" + name);
+    }
+}
+
+public class _SuperRender : IRenderEngine
+{
+    public void Render(string name)
+    {
+        Debug.Log("SuperRender绘制" + name);
+    }
+}
+
+/*
 public class _02Capsule
 {
     public string name = "Capsule";
@@ -58,3 +131,12 @@ public class _OpenGL
         Debug.Log("OpenGL绘制"+name);
     }
 }
+
+public class _DirectX
+{
+    public void Render(string name)
+    {
+        Debug.Log("_DirectX绘制" + name);
+    }
+}
+*/
