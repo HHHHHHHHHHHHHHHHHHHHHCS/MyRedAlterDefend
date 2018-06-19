7b016a354f34a8ef7f054be8455cc4e96f3ce54d
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-06-20 00:27:00 +0800

    创建地图

--- Assets/My/DesignMode/06Composite/_06Composite.cs
+++ Assets/My/DesignMode/06Composite/_06Composite.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class _06Composite : MonoBehaviour
 {
+    private void Awake()
+    {
+        DMCompsite root = new DMCompsite("Root");
 
+        DMLeaf leaf1 = new DMLeaf("GameObject");
+        DMLeaf leaf2 = new DMLeaf("GameObject (2)");
+        DMCompsite gameObject1 = new DMCompsite("GameObject (1)");
+
+        root.AddChild(leaf1);
+        root.AddChild(gameObject1);
+        root.AddChild(leaf2);
+
+        DMLeaf child1 = new DMLeaf("GameObject");
+        DMLeaf child2 = new DMLeaf("GameObject (1)");
+        gameObject1.AddChild(child1);
+        gameObject1.AddChild(child2);
+
+        ReadComponent(root);
+    }
+
+    private void ReadComponent(DMCompent component)
+    {
+        Debug.Log(component.Name);
+
+        var childList = component.ChildList;
+        if(childList==null|| childList.Count==0)
+        {
+            return;
+        }
+        foreach(var item in childList)
+        {
+            ReadComponent(item);
+        }
+    }
 }
 
 public abstract class DMCompent
 {
     public string Name { get; protected set; }
-    protected List<DMCompent> childList;
+    public List<DMCompent> ChildList { get; protected set; }
 
     public DMCompent(string _name)
     {
-        childList = new List<DMCompent>();
+        ChildList = new List<DMCompent>();
         Name = _name;
     }
 
@@ -49,21 +82,22 @@ public class DMCompsite : DMCompent
 {
     public DMCompsite(string _name) : base(_name)
     {
+
     }
 
     public override void AddChild(DMCompent _compent)
     {
-        childList.Add(_compent);
+        ChildList.Add(_compent);
     }
 
     public override DMCompent GetChild(int _index)
     {
-        return childList[_index];
+        return ChildList[_index];
     }
 
     public override void RemoveChild(DMCompent _compent)
     {
-        childList.Remove(_compent);
+        ChildList.Remove(_compent);
     }
 }
 
